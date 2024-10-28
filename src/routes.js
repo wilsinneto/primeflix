28400d252edb2acff707960c91ccde407b6ea447
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-10-28 17:10:07 -0300

    feat: created Header component

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
 
 function RoutesApp() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/filme/:id" element={<Filme />}></Route>
