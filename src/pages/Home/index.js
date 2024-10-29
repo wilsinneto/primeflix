bd860e10f9f5c387978f1535070f9af94ae9d165
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-10-29 17:21:14 -0300

    feat: fetched movie listing from api and integrated with Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,49 @@
+import "./home.css";
+
+import { useEffect, useState } from "react";
+
+import api from "../../services/api";
+import { Link } from "react-router-dom";
+
+// URL DA API: /movie/now_playing?api_key=7022985cba54443ac2a2d484383f6fff&language=pt-BR
+
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const { data } = await api.get("/movie/now_playing", {
+        params: {
+          api_key: "7022985cba54443ac2a2d484383f6fff",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+
+      console.log("data.results", data.results.slice(0, 10));
+
+      setFilmes(data.results.slice(0, 10));
+    }
+
+    loadFilmes();
+  }, []);
+
   return (
-    <div>
-      <h1>BEM VINDO A HOME</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
