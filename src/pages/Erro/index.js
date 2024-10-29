a371472b86ed291fbb39e78404b686953e527252
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-10-29 17:42:04 -0300

    feat: created Erro page

---
+++ src/pages/Erro/index.js
@@ -0,0 +1,15 @@
+import "./erro.css";
+
+import { Link } from "react-router-dom";
+
+function Erro() {
+  return (
+    <div className="not-found">
+      <h1>404</h1>
+      <h2>Página não encontrada!</h2>
+      <Link to="/">Veja todos filmes!</Link>
+    </div>
+  );
+}
+
+export default Erro;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Filme from "./pages/Filme";
+import Erro from "./pages/Erro";
 
 function RoutesApp() {
   return (
@@ -11,6 +12,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/filme/:id" element={<Filme />}></Route>
+        <Route path="*" element={<Erro />} />
       </Routes>
     </BrowserRouter>
   );
