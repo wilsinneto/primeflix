3b6aad3d41bb36a7e6a33263383d6c83bff2a3e7
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-10-30 16:30:09 -0300

    feat: created detail page

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,70 @@
+import "./filme-info.css";
+
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+import api from "../../services/api";
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "7022985cba54443ac2a2d484383f6fff",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("FILME NÃO ENCONTRADO");
+          return navigate("/", { replace: true });
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("COMPONENTE FOI DESMONTADO");
+    };
+  }, [id, navigate]);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>BEM VINDO A PÁGINA DETALHES DO FILME</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+
+      <h3>Sinopse</h3>
+      <span>{filme.overview}</span>
+
+      <strong>Avaliação: {filme.vote_average} / 10</strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 }
