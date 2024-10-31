b300925c92db80122527bbbd3aecf46972993880
Wilson Neto <wilson.neto@redesaoroque.com.br>
2024-10-31 15:58:55 -0300

    feat: added excluirFilme function

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -11,9 +11,21 @@ function Favoritos() {
 
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
+
+  function excluirFilme(id) {
+    const filtroFilmes = filmes.filter((item) => item.id !== id);
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus filmes</h1>
+
+      {filmes.length === 0 && (
+        <span>Você não possui nenhum filme salvo :(</span>
+      )}
 
       <ul>
         {filmes.map((item) => {
@@ -22,7 +34,7 @@ function Favoritos() {
               <span>{item.title}</span>
               <div>
                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
               </div>
             </li>
           );
